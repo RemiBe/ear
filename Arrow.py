bf6fe9f8cf6d8b7d74715209f1baa4a58e323341
Rémi Barat <remibarat@yahoo.fr>
2019-09-27 21:02:17 +0200

    Moving arrows

--- Arrow.py
+++ Arrow.py
@@ -7,21 +7,17 @@ class Arrow(object):
         self.algorator = algorator
         self.start = start
         self.to = to
+        start.arrows.append(self)
+        to.arrows.append(self)
         self.draw(start, to)
         self.saved = True
 
     def draw(self, start, to):
-        print("Arrow.draw from {} to {}".format(start, to))
         self.line = self.algorator.canvas.create_line(
                 start.cx,
                 start.cy,
                 to.cx,
                 to.cy)
-
-    def destroy(self):
-        canvas = self.algorator.canvas
-        canvas.delete(self.text)
-        canvas.delete(self.rect)
 
     def clicked_on(self, x, y):
         if self.start.cx < self.to.cx:
@@ -37,5 +33,13 @@ class Arrow(object):
             y1 = self.to.cy
             y2 = self.start.cy
         return x >= x1 and x <= x2 and y >= y1 and y <= y2
+
+    def move(self, event):
+        self.destroy()
+        self.draw(self.start, self.to)
+
+    def destroy(self):
+        canvas = self.algorator.canvas
+        canvas.delete(self.line)
 
 
